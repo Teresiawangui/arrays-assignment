3706138318ac99be175eeb9f17ae84451b318508
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-03-27 22:22:55 +0300

    arrays

--- arrays.js
+++ arrays.js
@@ -2,6 +2,10 @@
 //Find the last element of the following arrays.arr1 = [3,7,34,90,12]arr2 = [true, "green", "where",12,56]
 const arr1 = [3,7,34,90,12];
 const arr2 = [true,"green","where",12,56];
+console.log(arr1.slice(-1));
+console.log(arr2.slice(-1));
+
+
 const last =arr1[arr1.length -1];
 const lastElement =arr2[arr2.length -1];
 console.log({last});
@@ -11,6 +15,10 @@ console.log({lastElement});
 const pets = ["Cow","Bird","Snake","Dog"]
 const myPets = pets.join('');
 console.log({myPets});
+
+/*const pets = ["Cow","Bird","Snake","Dog"]
+const myPets = pets.join();
+console.log({myPets});*/
 //question3
 //Write a JS script to sort the following array itemsvar arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
 const arr3 = [-5,9,5,3,2,-3,6,8,4,1];
@@ -24,6 +32,10 @@ let arr = ["apple", "mango", "apple",  "orange", "mango", "mango"];
         index) => arr.indexOf(item) === index);
 }
 console.log(removeDuplicates(arr));
+
+/*function removeDuplicate(arr4){
+    return[...newSet(arr4)]
+}*/
 //question5
 //Write a JS script to search for the following word in the array."way"If the word is present, console it else console "the search word was not found"let arr5 = ["the", "way", "x", 4];
 
